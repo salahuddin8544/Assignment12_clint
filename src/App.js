1fef700cde2221bcb2e06a0c24a5c646e5756832
Salahuddin Ahmed <salahuddinahmed8544@gmail.com>
2022-11-24 16:51:55 -0800

    basic setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {RouterProvider} from 'react-router-dom'
+import router from './Routes/Route/Route';
+function App() {
+  return (
+    <div className="max-w-screen-xl	mx-auto">
+     <RouterProvider  router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Tis isfooter</h2>
+        </div>
+    );
+};
+
+export default Footer;
